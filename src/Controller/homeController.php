7d4fc154ccf6dc277a4c97845b62472eba9af286
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2024-05-03 16:10:53 +0000

    first commit

---
+++ src/Controller/homeController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+class homeController extends AbstractController
+    {
+        #[Route('/accueil', name: 'accueil')]
+        public function accueil(Request $request): Response
+        {
+           return $this->render('santart/accueil.html.twig');
+        }
+
+        #[Route('/about', name: 'about')]
+        public function about(Request $request): Response
+        {
+           return $this->render('santart/about.html.twig');
+        }
+
+        #[Route('/service', name: 'service')]
+        public function service(Request $request): Response
+        {
+        return $this->render('santart/service.html.twig');
+        }
+
+        #[Route('/shop', name: 'shop')]
+        public function shop(Request $request): Response
+        {
+           return $this->render('santart/shop.html.twig');
+        }
+        #[Route('/contact', name: 'contact')]
+        public function contact(Request $request): Response
+        {
+           return $this->render('santart/contact.html.twig');
+        }
+    }
